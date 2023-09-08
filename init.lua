d887267a4eae5ca4c017336a90dcc3fc8cac9d4e
Sergey Popov <quolpr@gmail.com>
2023-09-08 18:18:14 +0300

    Fix autoformat

--- init.lua
+++ init.lua
@@ -52,6 +52,72 @@ require('lazy').setup({
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
     },
+    config = function()
+      -- Switch for controlling whether you want autoformatting.
+      --  Use :KickstartFormatToggle to toggle autoformatting on or off
+      local format_is_enabled = true
+      vim.api.nvim_create_user_command('KickstartFormatToggle', function()
+        format_is_enabled = not format_is_enabled
+        print('Setting autoformatting to: ' .. tostring(format_is_enabled))
+      end, {})
+
+      -- Create an augroup that is used for managing our formatting autocmds.
+      --      We need one augroup per client to make sure that multiple clients
+      --      can attach to the same buffer without interfering with each other.
+      local _augroups = {}
+      local get_augroup = function(client)
+        if not _augroups[client.id] then
+          local group_name = 'kickstart-lsp-format-' .. client.name
+          local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+          _augroups[client.id] = id
+        end
+
+        return _augroups[client.id]
+      end
+
+      -- Whenever an LSP attaches to a buffer, we will run this function.
+      --
+      -- See `:help LspAttach` for more information about this autocmd event.
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
+        -- This is where we attach the autoformatting for reasonable clients
+        callback = function(args)
+          local client_id = args.data.client_id
+          local client = vim.lsp.get_client_by_id(client_id)
+          local bufnr = args.buf
+
+          -- Only attach to clients that support document formatting
+          if not client.server_capabilities.documentFormattingProvider then
+            return
+          end
+
+          -- Tsserver usually works poorly. Sorry you work with bad languages
+          -- You can remove this line if you know what you're doing :)
+          if client.name == 'tsserver' then
+            return
+          end
+
+          -- Create an autocmd that will run *before* we save the buffer.
+          --  Run the formatting command for the LSP that has just attached.
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = get_augroup(client),
+            buffer = bufnr,
+            callback = function()
+              if not format_is_enabled then
+                return
+              end
+
+              vim.lsp.buf.format {
+                async = false,
+                filter = function(c)
+                  return c.id == client.id
+                end,
+              }
+            end,
+          })
+        end,
+      })
+    end,
   },
 
   {
@@ -596,7 +662,7 @@ require('nvim-treesitter.configs').setup {
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', 'de', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', 'ds', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', 'dq', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 local navic = require 'nvim-navic'
@@ -648,8 +714,10 @@ vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(client, bufnr)
+  print(client.name)
   -- For other languages we are using null-ls
   if client.name ~= 'elixirls' then
+    print 'disaling formatting'
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
   end
@@ -709,9 +777,7 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  elixirls = {
-    cmd = { '/Users/quolpr/.elixir-ls/scripts/language_server.sh' },
-  },
+  elixirls = {},
   tsserver = {
     init_options = {
       -- This is the default which would be overwritten otherwise
@@ -858,52 +924,9 @@ local sources = {
   cspell.code_actions.with {},
 }
 
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-
-local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-  vim.lsp.buf_request(bufnr, 'textDocument/formatting', vim.lsp.util.make_formatting_params {}, function(err, res, ctx)
-    if err then
-      local err_msg = type(err) == 'string' and err or err.message
-      -- you can modify the log message / level (or ignore it completely)
-      vim.notify('formatting: ' .. err_msg, vim.log.levels.WARN)
-      return
-    end
-
-    -- don't apply results if buffer is unloaded or has been modified
-    if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, 'modified') then
-      return
-    end
-
-    if res then
-      local client = vim.lsp.get_client_by_id(ctx.client_id)
-      vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or 'utf-16')
-      vim.api.nvim_buf_call(bufnr, function()
-        vim.cmd 'silent noautocmd update'
-      end)
-    end
-  end)
-end
-
 null_ls.setup {
   debug = true,
   sources = sources,
-  -- you can reuse a shared lspconfig on_attach callback here
-  on_attach = function(client, bufnr)
-    if client.supports_method 'textDocument/formatting' then
-      if client.supports_method 'textDocument/formatting' then
-        vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-        vim.api.nvim_create_autocmd('BufWritePost', {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            async_formatting(bufnr)
-          end,
-        })
-      end
-    end
-  end,
 }
 
 local function lspSymbol(name, icon)

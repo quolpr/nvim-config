d5d829b31f7f2c9af3ad70903a55e5827bc4f6d1
Sergey Popov <quolpr@gmail.com>
2023-10-04 18:09:47 +0300

    Improve keybinds, remove typescript-tools

--- init.lua
+++ init.lua
@@ -484,11 +484,11 @@ require('lazy').setup({
     },
   },
   -- It should give better TS perfromance
-  {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-  },
+  -- {
+  --   'pmizio/typescript-tools.nvim',
+  --   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  --   opts = {},
+  -- },
 }, {})
 
 -- [[ Setting options ]]
@@ -785,22 +785,22 @@ local on_attach = function(client, bufnr)
   end
 
   -- nmap('-', '<cmd>Neotree source=filesystem reveal=true position=current <CR>', 'Open neotree')
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>cc', vim.lsp.buf.rename, '[C]ode [C]hange')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('gd', vim.lsp.buf.definition, '[C]ode Goto [D]efinition')
+  nmap('gr', vim.lsp.buf.references, '[C]ode Goto [R]eferences')
+  nmap('gi', vim.lsp.buf.implementation, '[C]ode Goto [I]mplementation')
+  nmap('gD', vim.lsp.buf.type_definition, '[C]ode Type [D]efinition')
+  nmap('<leader>cs', require('telescope.builtin').lsp_document_symbols, '[C]ode [s]ymbols')
+  nmap('<leader>cS', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[C]code Workspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>cd', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
@@ -823,16 +823,16 @@ local servers = {
   -- rust_analyzer = {},
   elixirls = {},
   -- nextls = {},
-  -- tsserver = {
-  --   init_options = {
-  --     -- This is the default which would be overwritten otherwise
-  --     hostInfo = 'neovim',
-  --     -- 16 gb
-  --     maxTsServerMemory = 16384,
-  --     -- Never use LSP for syntax anyway
-  --     tsserver = { useSyntaxServer = 'never' },
-  --   },
-  -- },
+  tsserver = {
+    init_options = {
+      -- This is the default which would be overwritten otherwise
+      hostInfo = 'neovim',
+      -- 16 gb
+      maxTsServerMemory = 16384,
+      -- Never use LSP for syntax anyway
+      tsserver = { useSyntaxServer = 'never' },
+    },
+  },
   sqlls = {},
   eslint = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
@@ -873,15 +873,28 @@ mason_lspconfig.setup_handlers {
   end,
 }
 
-require('typescript-tools').setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = require('lspconfig').util.root_pattern '.git',
-  settings = {
-    separate_diagnostic_server = true,
-    tsserver_max_memory = 'auto',
-  },
-}
+-- require('typescript-tools').setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   root_dir = require('lspconfig').util.root_pattern '.git',
+--   settings = {
+--     separate_diagnostic_server = true,
+--     tsserver_max_memory = 'auto',
+--   },
+-- }
+
+-- Additional mason installation
+local mason = require 'mason-registry'
+
+local pkgs = { 'prettierd', 'cspell' }
+
+for i, package_name in ipairs(pkgs) do
+  local pkg = mason.get_package(package_name)
+
+  if not pkg:is_installed() then
+    pkg:install()
+  end
+end
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`

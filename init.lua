639acb4fa7f073c10a26037eb52f8bf8b9a16fc7
Sergey Popov <quolpr@gmail.com>
2024-08-06 12:24:29 +0300

    Improve performance

--- init.lua
+++ init.lua
@@ -1424,56 +1424,57 @@ require('lazy').setup {
             -- -- scope_incremental = '<c-S>',
           },
         },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              -- a = means argument
-              ['ia'] = '@parameter.inner',
-              ['aa'] = '@parameter.outer',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-              ['ii'] = '@conditional.inner',
-              ['ai'] = '@conditional.outer',
-              ['il'] = '@loop.inner',
-              ['al'] = '@loop.outer',
-              ['at'] = '@comment.outer',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']f'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']F'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[f'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[F'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
-          },
-          -- swap = {
-          --   enable = true,
-          --   swap_next = {
-          --     ['<leader>a'] = '@parameter.inner',
-          --   },
-          --   swap_previous = {
-          --     ['<leader>A'] = '@parameter.inner',
-          --   },
-          -- },
-        },
+        -- Slow downs vim https://github.com/nvim-treesitter/nvim-treesitter/issues/5868
+        -- textobjects = {
+        --   select = {
+        --     enable = true,
+        --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        --     keymaps = {
+        --       -- You can use the capture groups defined in textobjects.scm
+        --       -- a = means argument
+        --       ['ia'] = '@parameter.inner',
+        --       ['aa'] = '@parameter.outer',
+        --       ['af'] = '@function.outer',
+        --       ['if'] = '@function.inner',
+        --       ['ac'] = '@class.outer',
+        --       ['ic'] = '@class.inner',
+        --       ['ii'] = '@conditional.inner',
+        --       ['ai'] = '@conditional.outer',
+        --       ['il'] = '@loop.inner',
+        --       ['al'] = '@loop.outer',
+        --       ['at'] = '@comment.outer',
+        --     },
+        --   },
+        --   move = {
+        --     enable = true,
+        --     set_jumps = true, -- whether to set jumps in the jumplist
+        --     goto_next_start = {
+        --       [']f'] = '@function.outer',
+        --       [']]'] = '@class.outer',
+        --     },
+        --     goto_next_end = {
+        --       [']F'] = '@function.outer',
+        --       [']['] = '@class.outer',
+        --     },
+        --     goto_previous_start = {
+        --       ['[f'] = '@function.outer',
+        --       ['[['] = '@class.outer',
+        --     },
+        --     goto_previous_end = {
+        --       ['[F'] = '@function.outer',
+        --       ['[]'] = '@class.outer',
+        --     },
+        --   },
+        --   -- swap = {
+        --   --   enable = true,
+        --   --   swap_next = {
+        --   --     ['<leader>a'] = '@parameter.inner',
+        --   --   },
+        --   --   swap_previous = {
+        --   --     ['<leader>A'] = '@parameter.inner',
+        --   --   },
+        --   -- },
+        -- },
       }
 
       -- There are additional nvim-treesitter modules that you can use to interact

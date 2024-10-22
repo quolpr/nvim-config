681d66aa3aacd2b2ba69548744e420a2805876a2
Sergey Popov <quolpr@gmail.com>
2024-10-22 13:46:14 +0400

    Add explore-code

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ mkdir -p ~/.local/bin
 ln ./tmux-sessionizer ~/.local/bin/tmux-sessionizer
 ln ./tmux-nvim ~/.local/bin/tmux-nvim
 ln ./tmux-zettel ~/.local/bin/tmux-zettel
+ln ./explore-code ~/.local/bin/explore-code
 
 npm i -g @cspell/cspell-bundled-dicts @cspell/dict-ru_ru @cspell/dict-golang @cspell/dict-software-terms
 

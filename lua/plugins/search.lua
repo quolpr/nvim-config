5f853ce900b2cb4fb1ce256f33c95ef9b6963a41
Sergey Popov <quolpr@gmail.com>
2024-11-25 11:08:16 +0300

    Improve search

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -173,14 +173,23 @@ return {
     end,
   },
   {
-    'nvim-pack/nvim-spectre',
+    'MagicDuck/grug-far.nvim',
+    opts = {},
     keys = {
       {
         '<leader>fp',
         function()
-          require('spectre').open()
+          require('grug-far').open()
         end,
         desc = 'Find and Replace in Project',
+      },
+      {
+        '<leader>fp',
+        function()
+          require('grug-far').with_visual_selection()
+        end,
+        desc = 'Find and Replace in Project',
+        mode = 'v',
       },
     },
   },
